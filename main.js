4ab0117e6b40f4777591b40b217e00166bda897c
michellifernanda <dreamsbigmf@gmail.com>
2023-02-02 12:00:44 -0300

    comit inicial

---
+++ main.js
@@ -0,0 +1,18 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('La Capital del Sol')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
+
+  $('.carousel').carousel({
+    interval: 3000
+  });
+  
